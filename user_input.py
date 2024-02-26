31d0a922c29437aec6c4dacd2cba2a5ce9494ba9
Glen-Yegon <yegonglen@gmail.com>
2024-02-26 19:09:26 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age  = input("What is your age? ")
+location = input("Where do you live? ")
+print("My name is " + name , ", I am " + age , "years old, and I live in " + location) 
