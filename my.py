d46a9ffc121fd43d87179f3034925e39e58fbe4c
Glen-Yegon <yegonglen@gmail.com>
2024-03-01 02:43:03 +0300

    Glen

---
+++ my.py
@@ -0,0 +1,17 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list.insert(1, 15)
+print(my_list)
+my_list2 = [50, 60, 70]
+my_list = my_list + my_list2
+print(my_list)
+my_list.remove(70)
+print(my_list)
+my_list.sort()
+print(my_list)
+index = my_list.index(30)
+print(index)
