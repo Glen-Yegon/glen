6d0711b9256cd83300eef52c4d7569dd16e5167d
Glen-Yegon <yegonglen@gmail.com>
2024-03-04 23:33:24 +0300

    Dart Variables

---
+++ main.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  //Data types in dart
+  int age = 20;
+  //int stores intager values
+  double weight = 70.5;
+  //double stores numbers with decimal points
+  String name = "Glen";
+  //string stores characters
+  bool is_Married = true;
+  //bool represends true or false values
+  List<String> names = ["John", "Glen"];
+  //list is an ordered group of objects
+  Map<String, int> ages = {"Glen": 30};
+  //maps represent a set of values as key value pairs
+  print("My name is $name, I am $age years old and I weigh $weight Kgs.");
+}
